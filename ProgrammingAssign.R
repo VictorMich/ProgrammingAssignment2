3a6a0a48e288b788462f0bfe0bf403c4d8c3e743
Victor Michel <43556743+VictorMich@users.noreply.github.com>
2020-08-22 20:07:50 -0500

    Add files via upload

--- ProgrammingAssign.R
+++ ProgrammingAssign.R
@@ -5,15 +5,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()){
-  i <- NULL 
-  set <- function(y) {
-    x <<- y
-    i <<- NULL
+  i <- NULL ##Value of the inverse of the matrix
+  set <- function(y) { ##Assigning a new variable with function(y)
+    x <<- y ##Value of Matrix
+    i <<- NULL ##If it's a new matrix, reset the value to NULL
   }
-  get <- function() x
-  setinverse <- function(inverse) i <<- inverse
-  getinverse <- function() i
-  list(set = set,
+  get <- function() x #New variable with function(x)
+  setinverse <- function(inverse) i <<- inverse #Assigns a new variable with function(inverse)
+  getinverse <- function() i ##Gets the inverse 
+  list(set = set, ##Need this to run de function within the list
        get = get,
        setinverse = setinverse,
        getinverse = getinverse)
@@ -23,12 +23,12 @@ makeCacheMatrix <- function(x = matrix()){
 
 cacheSolve <- function(x, ...) {
     ##Returns a matrix that is the inverse of "x"
-  i <- x$getinverse()
-  if (!is.null(i)) {
+  i <- x$getinverse() ##Variable that gets the inverse
+  if (!is.null(i)) { ##Checks if the inverse has already been calculated
     message("getting cached data")
     return(i)
   }
-  data <- x$get()
+  data <- x$get() ##Solves the inverse
   i <- solve(data, ...)
   x$setinverse(i)
   i
